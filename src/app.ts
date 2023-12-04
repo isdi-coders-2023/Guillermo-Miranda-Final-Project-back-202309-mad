e133a23c654d86cfa250ff18fbb9183ed619d745
Guille <g.mirandagaleote@gmail.com>
2023-12-04 21:18:02 +0100

    Add conexion with mongo db, a new server with the port up to listen and express app

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
+import cors from 'cors';
+import express from 'express'
+import morgan from 'morgan';
+import createDebug from 'debug';
+// Import { usersRouter } from './routers/user.router.js';
+
+const debug = createDebug('FPB:app');
+
+export const app = express();
+debug('Starting');
+
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.static('public'));
+
+
+// App.use('/users', usersRouter); 
 
