8e6fead71785166585e2fd52f18ce74d4c6c8275
Guille <g.mirandagaleote@gmail.com>
2023-12-04 19:06:41 +0100

    Add entitie and schema of users

---
+++ src/repo/repo.users/users.mongo.model.ts
@@ -0,0 +1,31 @@
+import { UserStructure } from "../../entities/user.js";
+import { Schema, model } from "mongoose";
+
+const usersSchema = new Schema<UserStructure>({
+  email:{
+    type: String,
+    required: true,
+    unique:  true
+  },
+  passwd:{
+    type: String,
+    required: true
+  },
+  userName:{
+    type: String,
+    required: true,
+  },
+  styleFood:String,
+  descriptionUser: String
+})
+
+usersSchema.set('toJSON', {
+  transform(_document, returnedObject) {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwd;
+  },
+});
+
+export const UserModel = model('User', usersSchema, 'users');
